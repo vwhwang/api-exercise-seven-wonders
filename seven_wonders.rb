e9a2791c9bfe2cddaebf11dda738ff36b06ddd91
vwhwang <vwhwang38@gmail.com>
2020-03-09 15:40:17 -0700

    completed and added env file with api_key

--- seven_wonders.rb
+++ seven_wonders.rb
@@ -1,11 +1,25 @@
 require 'httparty'
+require 'dotenv'
 
-BASE_URL = "THE BASE URL FOR THE API REQUEST"
-LOCATION_IQ_KEY = "YOUR API TOKEN"
+Dotenv.load 
+
+BASE_URL = "https://us1.locationiq.com/v1/search.php?key=YOUR_PRIVATE_TOKEN&q=SEARCH_STRING&format=json"
+LOCATION_IQ_KEY = ENV["API_KEY"]
 
 def get_location(search_term)
+  query_parameters = {
+    key: LOCATION_IQ_KEY,
+    q: search_term
+  }
 
+  response = HTTParty.get(BASE_URL, query: query_parameters)
+  result = {}
+  result[search_term] = {:lat => response[0]["lat"].to_s, :lon => response[0]["lon"].to_s}
+  return result
+  
 end
+
+
 
 def find_seven_wonders
 
@@ -22,5 +36,6 @@ def find_seven_wonders
 end
 
 puts "#{find_seven_wonders}"
+
 # Expecting something like:
 # [{"Great Pyramid of Giza"=>{:lat=>"29.9791264", :lon=>"31.1342383751015"}}, {"Gardens of Babylon"=>{:lat=>"50.8241215", :lon=>"-0.1506162"}}, {"Colossus of Rhodes"=>{:lat=>"36.3397076", :lon=>"28.2003164"}}, {"Pharos of Alexandria"=>{:lat=>"30.94795585", :lon=>"29.5235626430011"}}, {"Statue of Zeus at Olympia"=>{:lat=>"37.6379088", :lon=>"21.6300063"}}, {"Temple of Artemis"=>{:lat=>"32.2818952", :lon=>"35.8908989553238"}}, {"Mausoleum at Halicarnassus"=>{:lat=>"37.03788265", :lon=>"27.4241455276707"}}]
